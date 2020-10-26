9cc4e60fb5dc18c6c105c096213868298c26f86c
Chris <61188486+chris-clearence@users.noreply.github.com>
2020-10-26 12:29:23 +0000

    1
    Signed-off-by: Chris <61188486+chris-clearence@users.noreply.github.com>

--- var/www/html/.files/1/index.php
+++ var/www/html/.files/1/index.php
@@ -2,7 +2,8 @@
 
 $file = file_get_contents('/var/www/dump/files.hostname');
 
-echo "<h1 style=\"text-align:center;\">Hello World.</h1><br><p>Wondering what to do now? Why not upload your web files at <a href=\"http://".$file."/info.server.php\" target=\"_blank\">".$file."</a>, and read the manual. Have fun.</p>";
+echo '<link rel="shortcut icon" type="image/png" href="'.$file.'/Curious2Hosting.png" id="favicon" />';
+echo "<h1 style=\"text-align:center;\">Hello World.</h1><br><p>Wondering what to do now? Go to <a href=\"http://".$file."/info.server.php\" target=\"_blank\">".$file."</a>, and read the server info page. Have fun.</p>";
 echo "<p>Powered by Hidden Hosting - Contact me for help (curious@null.net)</p>";
 
 ?>
